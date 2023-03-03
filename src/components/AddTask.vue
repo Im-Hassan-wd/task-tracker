b3a0032d4469e2c892e304b59f36cc94cc945f33
Taiwo Sodiq <alhassansahih@gmail.com>
2023-03-03 20:31:03 +0100

    form

---
+++ src/components/AddTask.vue
@@ -0,0 +1,30 @@
+<template>
+  <form>
+    <div class="form-control">
+      <label>Task</label>
+      <input type="text" name="text" placeholder="Add Task">
+    </div>
+    
+    <div class="form-control">
+      <label>Date & Time</label>
+      <input type="text" name="date" placeholder="March 24 at 3:00am">
+    </div>
+
+    <div class="form-control form-control-check">
+      <label>Set Reminder</label>
+      <input type="check-box" name="reminder">
+    </div>
+
+    <input type="submit" value="Save Task" class="btn btn-block">
+  </form>
+</template>
+
+<script>
+export default {
+    name: "AddTask",
+}
+</script>
+
+<style>
+
+</style>
