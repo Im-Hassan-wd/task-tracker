06bdc35a813ee47ab3fb565a5df4ee20126d0a96
Taiwo Sodiq <alhassansahih@gmail.com>
2023-03-04 22:35:47 +0100

    vue config proxy

--- src/App.vue
+++ src/App.vue
@@ -38,37 +38,24 @@ export default {
     addTask(task) {
       this.tasks = [...this.tasks, task]
     },
-    toggleAddTask() {
+    toggleAddTasks() {
       this.showAddTask = !this.showAddTask
     },
     async fetchTask() {
-      const res = await fetch("localhost:5000/tasks")
+      const res = await fetch("api/tasks")
+      const data = await res.json()
+
+      return data
+    },
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks${id}`)
       const data = await res.json()
 
       return data
     },
   },
-  created() {
-    this.tasks = [
-      {
-        id: 1,
-        text: 'Doctors Appointement',
-        date: 'March 1st at 2:30pm',
-        reminder: false
-      },
-      {
-        id: 2,
-        text: 'Meeting at School',
-        date: 'March 3rd at 5:30am',
-        reminder: true
-      },
-      {
-        id: 3,
-        text: 'Play MiniMilitia',
-        date: 'Every goddem day',
-        reminder: true
-      }
-    ]
+  async created() {
+    this.tasks = await this.fetchTasks()
   }
 }
 </script>
